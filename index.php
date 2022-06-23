2bfc3fbe34925437c7abff833bd1c3cfa1e97b12
CyskoFR <franc.sko91@gmail.com>
2022-06-23 14:24:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!-- Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack1</title>
+</head>
+<body>
+    <h1>Snack1</h1>
+    <br>
+    <?php foreach($posts as $date => $articles) { ?>
+        <h4><?= "Date: {$date}" ?></h4>
+        <?php foreach($articles as $article) { ?>
+            <h2><?= $article['title'] ?></h2>
+            <h3><?= $article['author'] ?></h3>
+            <p><?= $article['text'] ?></p>
+            <br>
+        <?php } ?>
+        <hr>
+    <?php } ?>
+</body>
+</html>
